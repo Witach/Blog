4cfe7669cca605811593d2b411ea46d7ff7862df
Witach <witaszek98@wp.pl>
2019-02-04 21:56:40 +0100

    PostView added

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+from django.urls import reverse_lazy
 from app.models import Post
 from django.utils import timezone
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView,ListView,DetailView,CreateView,UpdateView,DeleteView
 # Create your views here.
 class startView(TemplateView):
@@ -14,3 +16,20 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
     model = Post
+
+class PoostCreateView(LoginRequiredMixin,CreateView):
+    model = Post
+    login_url='/login/'
+    redirect_field_name = "post_detail.html"
+    form_class = PostForm
+
+class PostUpdateView(LoginRequiredMixin,UpdateView):
+    model = Post
+    login_url='/login/'
+    redirect_field_name = "post_detail.html"
+    form_class = PostForm
+
+class PostDeleteView(LoginRequiredMixin,DeleteView):
+    model = Post
+    login_url='/login/'
+    succes_url="post_list"
