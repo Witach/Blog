fbc7bb9afd7c5c4af32708b9164e825bed1b74a3
Witach <witaszek98@wp.pl>
2019-02-08 22:19:14 +0100

    style.css added

--- Blog/settings.py
+++ Blog/settings.py
@@ -14,6 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+print(BASE_DIR)
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 STATIC_DIR = os.path.join(BASE_DIR,'static')
 # Quick-start development settings - unsuitable for production
@@ -120,4 +121,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
-STATIC_ROOT = STATIC_DIR
+STATICFILES_DIRS = [ STATIC_DIR,]
